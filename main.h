b9c06bd52d8195b8fdd15344a26750e16e2cdc60
Markvanderlee <Markvanderlee95@hotmail.com>
2017-05-11 13:27:19 +0200

    Add files via upload

---
+++ main.h
@@ -0,0 +1,26 @@
+//--------------------------------------------------------------
+// File     : main.h
+//--------------------------------------------------------------
+
+//--------------------------------------------------------------
+#ifndef __STM32F4_UB_MAIN_H
+#define __STM32F4_UB_MAIN_H
+
+
+//--------------------------------------------------------------
+// Includes
+//--------------------------------------------------------------
+#include "stm32f4xx.h"
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_syscfg.h"
+#include "stm32f4xx_pwr.h"
+#include "stm32f4xx_usart.h"
+#include "misc.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+
+//--------------------------------------------------------------
+#endif // __STM32F4_UB_MAIN_H
